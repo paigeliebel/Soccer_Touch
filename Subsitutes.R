90e64e8f547338cf4d334f479b9d092d91ae5088
Liebel <csbc1547@uibk.ac.at>
2025-05-27 11:26:08 +0200

    sub script

---
+++ Subsitutes.R
@@ -0,0 +1,3 @@
+#Substitutes: see if it is a control, do touchier teams also touch more during subs?
+#Use this data set and run it through the other scripts to see if it is consistent
+#Control for number of subs
