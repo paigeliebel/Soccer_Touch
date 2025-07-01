c76414cbdf262f93d0af24765ca0bc297c8dbfd0
paigeliebel <paigeliebel@gmail.com>
2025-07-01 12:53:57 +0200

    delete empty doc

--- Subsitutes.R
+++
@@ -1,3 +0,0 @@
-#Substitutes: see if it is a control, do touchier teams also touch more during subs?
-#Use this data set and run it through the other scripts to see if it is consistent
-#Control for number of subs
